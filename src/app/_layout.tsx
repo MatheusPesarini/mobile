c8781dd488448decb73dd1831af04da30965c189
Matheus <mrogeriopesarini@gmail.com>
2025-01-08 23:22:18 -0300

    Adicionar integração com API, componentes de categorias e lugares, além de ajustes de estilo e navegação

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import { colors } from "@/styles/theme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import {
 	useFonts,
@@ -12,23 +13,25 @@ import {
 import { Loading } from "@/components/loading";
 
 export default function Layout() {
-	 const [fontsLoaded] = useFonts({
+	const [fontsLoaded] = useFonts({
 		Rubik_600SemiBold,
 		Rubik_400Regular,
 		Rubik_500Medium,
 		Rubik_700Bold,
 	});
 
-  if(!fontsLoaded) {
-    return <Loading />;
-  }
+	if (!fontsLoaded) {
+		return <Loading />;
+	}
 
 	return (
-		<Stack
-			screenOptions={{
-				headerShown: false,
-				contentStyle: { backgroundColor: colors.gray[100] },
-			}}
-		/>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+					contentStyle: { backgroundColor: colors.gray[100] },
+				}}
+			/>
+		</GestureHandlerRootView>
 	);
 }
---
+++ src/app/home.tsx
@@ -0,0 +1,57 @@
+import { View, Alert } from "react-native";
+import { useEffect, useState } from "react";
+import { api } from "@/api/api";
+
+import { Categories, type CategoriesProps } from "@/components/categories";
+import type { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+type MarketProps = PlaceProps;
+
+export default function Home() {
+	const [categories, setCategories] = useState<CategoriesProps>();
+	const [category, setCategory] = useState("");
+	const [markets, setMarkets] = useState<MarketProps[]>([]);
+
+	async function fetchCategories() {
+		try {
+			const { data } = await api.get("/categories");
+			setCategories(data);
+			setCategory(data[0].id);
+		} catch (error) {
+			console.log(error);
+			Alert.alert("Categorias", "Não foi possível carregar as categorias");
+		}
+	}
+
+	async function fetchMarkets() {
+		try {
+			if (!category) return;
+
+			const { data } = await api.get(`/markets/category/${category}`);
+			setMarkets(data);
+		} catch (error) {
+			console.log(error);
+			Alert.alert("Mercados", "Não foi possível carregar os mercados");
+		}
+	}
+
+	useEffect(() => {
+		fetchCategories();
+	}, []);
+
+	useEffect(() => {
+		fetchMarkets();
+	}, [category]);
+
+	return (
+		<View style={{ flex: 1, backgroundColor: "#CECECE" }}>
+			<Categories
+				data={categories}
+				onSelect={setCategory}
+				selected={category}
+			/>
+			<Places data={markets} />
+		</View>
+	);
+}
